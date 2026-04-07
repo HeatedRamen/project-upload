29169a041e4052fcae3fe9af269620e368ac6755
ramenmac <kevinnguyen4200@gmail.com>
2026-04-07 15:57:49 -0500

    Create follow-up print statement

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("I hope you're having a good day!");
     }
 }
